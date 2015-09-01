f806200ddb39a60ca0ef5784bf4450ea30e0f21f
alex cai <cyy0523xc@gmail.com>
2015-09-01 15:18:08 +0800

    install docker

--- install-docker.sh
+++ install-docker.sh
@@ -3,6 +3,9 @@
 wget -qO- https://get.docker.com/gpg | sudo apt-key add -
 wget -qO- https://get.docker.com/ | sh
 
+# install pip
+sudo apt-get install python-pip
+
 # docker-compose
 sudo pip install -U docker-compose
 sudo chmod +x /usr/local/bin/docker-compose 
