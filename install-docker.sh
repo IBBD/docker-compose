f077a24648bc3e23b21afe66d0a87634d0388411
alex cai <cyy0523xc@gmail.com>
2015-09-02 11:08:07 +0800

    fixed 阿里云

--- install-docker.sh
+++ install-docker.sh
@@ -13,3 +13,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker -v
 docker-compose -v
 
+# 在阿里云上得修改一下路由表 
+# @see http://segmentfault.com/q/1010000000438713
+#sudo route del -net 172.16.0.0 netmask 255.240.0.0  
+
