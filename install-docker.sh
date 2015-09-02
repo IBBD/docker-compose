d3be23bd9f757ee879e654d9de084a4ec855baf5
alex cai <cyy0523xc@gmail.com>
2015-09-02 11:15:22 +0800

    fixed 阿里云

--- install-docker.sh
+++ install-docker.sh
@@ -11,9 +11,11 @@ sudo pip install -U docker-compose
 sudo chmod +x /usr/local/bin/docker-compose 
 
 docker -v
+docker info
 docker-compose -v
 
 # 在阿里云上得修改一下路由表, 因为 docker 的虚拟子网是在这个 prefix 里的 
+# 否则可能会出现错误：connect to the Docker daemon. Is 'docker -d' running on this host?
 # @see http://segmentfault.com/q/1010000000438713
 #sudo route del -net 172.16.0.0 netmask 255.240.0.0  
 
