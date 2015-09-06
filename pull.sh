02026b16d36a2cff6016dc8645a474dfa7d548e5
alex cai <cyy0523xc@gmail.com>
2015-09-06 14:28:01 +0800

    pull.sh

---
+++ pull.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# sudo ./pull.sh nginx
+# sudo ./pull.sh php-fpm
+
+source ./.env.sh 
+
+docker pull $docker_registry:5000/ibbd/$1
+docker tag $docker_registry:5000/ibbd/$1 ibbd/$1
+docker rmi $docker_registry:5000/ibbd/$1
+
+echo "===> pull ibbd/$1 had done!"
